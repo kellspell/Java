688b8e2111efb93c580d011678b41cbf096d173e
Kellspell <kskeelmadrid3@gmail.com>
2025-02-05 22:37:01 +0000

    my first

---
+++ First-Test/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // This is my first Java program
+        System.out.print("I love Pizza!\n");
+        System.out.print("Its really Good!");
+    }
+}
